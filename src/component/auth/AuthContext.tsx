a466f9fd96b4be4b68107825a453a7518a4d7f3b
linhnnase170113 <133214149+linhnnase170113@users.noreply.github.com>
2023-07-05 22:05:34 +0700

    nnn

--- src/component/auth/AuthContext.tsx
+++ src/component/auth/AuthContext.tsx
@@ -16,6 +16,7 @@ export const UserContext = createContext(userInit);
 
 export default function AuthProvider({ children }: any) {
   const [open, setOpen] = useState<boolean>(false)
+  const router = useRouter()
   const openLoading = open
   const setOpenLoading = (openLoading : boolean) => {
     setOpen(openLoading)
@@ -31,7 +32,9 @@ export default function AuthProvider({ children }: any) {
     try {
       if (currentUser !== null) {
         const user = await UseLogin({ userUid: currentUser.uid })
+        if (user.data?.userRole === 1) router.push("/admin")
         if (user.data !== null) {
+          setOpenLoading(true)
           const data = await UseGetCartUserUid({
             userUid: currentUser.uid
           })
@@ -42,6 +45,8 @@ export default function AuthProvider({ children }: any) {
       }
     } catch (error: any) {
       console.log(error)
+    } finally {
+      setOpenLoading(false)
     }
   };
   useEffect(() => {
--- src/pages/order/[...slug].tsx
+++ src/pages/order/[...slug].tsx
@@ -1,22 +1,48 @@
-import { GetServerSidePropsContext } from 'next';
-import React from 'react'
+import Layout2 from "@/component/theme/layout/Layout2";
+import { GetServerSideProps } from "next";
+import React, { useEffect } from "react";
+import { UseLogin } from "../../../package/function/auth/use-login";
+import { ResponseBody } from "../../../package/model/api";
+import { User } from "../../../package/model/user";
+import { UseGetOrderHook } from "../../../package/function/order/get-hook";
+import OrderTable from "@/component/order/OrderTable";
+import OrderTab from "@/component/order/OrderTab";
 
-export async function getServerSideProps({
-    params,
-}: GetServerSidePropsContext) {
-    const slug = params?.slug
-
-    console.log(slug)
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const slug = params?.slug;
+  const response = await fetch(
+    "http://localhost:8080/all/orderstatus/allOrderStatus"
+  );
+  const allOrderStatus = await response.json();
+  if (slug !== undefined) {
+    const user: ResponseBody<User> = await UseLogin({ userUid: slug[0] });
     return {
-        props: {
-        },
+      props: {
+        orderStatus: allOrderStatus,
+        user: user.data,
+      },
     };
-}
+  }
+  return {
+    props: {
+      orderStatus: allOrderStatus,
+      user: {},
+    },
+  };
+};
 
-const Order = () => {
-    return (
-        <div>Order</div>
-    )
+export default function Order({ user, orderStatus }: { user: User, orderStatus : any }) {
+  const { data, isLoading, mutate } = UseGetOrderHook({
+    userId: user.userId,
+  });
+  useEffect(() => {
+    setInterval(() => {
+      mutate()
+    }, 60000)
+  })
+  return (
+    <Layout2>
+      <OrderTab orderStatusList={orderStatus} data={data?.data}/>
+    </Layout2>
+  );
 }
-
-export default Order
